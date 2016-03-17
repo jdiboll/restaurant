857d9442714e3441c3b3dc0e3d457adc18b40c82
Heather Conley <hconley6695@gmail.com>
2016-03-16 22:55:46 -0400

    added template functions for forms, both reservation and comment forms. error coming back regarding import/export but may be because our array is within the template file. commented out the word "export" and got no erros when savings

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 // Javascript Entry Point
+// Javascript Entry Point
+import $ from 'jquery';
+
+import form_data from './template_for_forms';
+
+import { lineOfText, commentSection, selectSeat } from './template_for_forms';
+
+
+var $app = $('.app');
+
+form_data.forEach(function(datum) {
+	var template;
+	if (datum.type === "text" || datum.type === "email" || datum.type === "tel" || datum.type === "date") {
+		template = lineOfText;
+	} else if (datum.type === "textarea") {
+		template = commentSection;
+	} else if (datum.type === "select") {
+		template = selectSeat;
+	}
+	var html = template(datum);
+	$app.append(html);
+});
