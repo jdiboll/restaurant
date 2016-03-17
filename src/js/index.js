3f929cb6ead21f5f62eebaaaaa5a09f24393ad51
John Cooley <johndavidcooley@gmail.com>
2016-03-17 16:35:27 -0400

    Committing before grabbing more images.

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,26 @@
 import $ from 'jquery';
 
-import form_data from './template_for_forms';
+// import form_data from './template_for_forms';
 
-import { lineOfText, commentSection, selectSeat } from './template_for_forms';
+// import { lineOfText, commentSection, selectSeat } from './template_for_forms';
 
 
-// 
+// // 
 
-var $app = $('.app');
+// var $app = $('.app');
 
-form_data.forEach(function(datum) {
-	var template;
-	if (datum.type === "text" || datum.type === "email" || datum.type === "tel" || datum.type === "date") {
-		template = lineOfText;
-	} else if (datum.type === "textarea") {
-		template = commentSection;
-	} else if (datum.type === "select") {
-		template = selectSeat;
-	}
-	var html = template(datum);
-	$app.append(html);
-});
+// form_data.forEach(function(datum) {
+// 	var template;
+// 	if (datum.type === "text" || datum.type === "email" || datum.type === "tel" || datum.type === "date") {
+// 		template = lineOfText;
+// 	} else if (datum.type === "textarea") {
+// 		template = commentSection;
+// 	} else if (datum.type === "select") {
+// 		template = selectSeat;
+// 	}
+// 	var html = template(datum);
+// 	$app.append(html);
+// });
 
 
 
