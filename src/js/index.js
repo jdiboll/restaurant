f44b16ad371d51c08bc2b3ba4bfe3128375daccd
John Cooley <johndavidcooley@gmail.com>
2016-03-17 10:47:31 -0400

    Initial commit from John.
    The HTML shell and SASS are close to complete. I'm attempting to pull something before I push my changes to GitHub.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
 // Javascript Entry Point
+
+import $ from 'jquery';
+
+// import { getYear, getSeconds } from './current_year'
+
+// news
+
+
+// special_api
+
+
+// google_maps
+
+
+// menu
+
+
+// reservations
+
+
+// reviews
+
+
+// current_year
+
+function nowSeconds() {
+	var today;
+	var currentSeconds;
+
+	today = new Date();
+	currentSeconds = today.getSeconds();
+
+	var copyrightSeconds = document.querySelector('.copyright')
+
+ 	copyrightSeconds.innerHTML = `&copy; ${currentSeconds} Ye Olde.`;
+ };
+
+ var intID = window.setInterval(nowSeconds, 1000);
