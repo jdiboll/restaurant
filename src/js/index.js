fad62e9ce3111910fda44568ff7a876a1e35a34d
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-18 13:12:17 -0400

    Pushing so Josh can pull

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
- 
+
 // import formArray from './template_for_forms';
 
 // import form_data from './template_for_forms';
@@ -24,7 +24,7 @@ import $ from 'jquery';
 //	$app.append(html);
 //	return html;
 //});
- 
+
 
 import { formArray, lineOfText, commentSection, selectSeat } from './template_for_forms';
 
@@ -80,65 +80,12 @@ $.ajax(url).then(function(data) {
 
 
 
-<<<<<<< HEAD
-// import $ from 'jquery';
-
-
-// var url = 'https://json-data.herokuapp.com/restaurant/news/1';
-
-// $.ajax(url).then(function(data){
-// 	$('#news').append(`
-// 		<div class="news_wrapper">
-// 			<div class="news_title">${data.title}</div>
-// 			<div class="news_date">${data.date_published}</div>
-// 			<div class="news_post">${data.post}</div>
-// 		</div>
-// 		`);
-// });
-
-
-
-
-
-
-// $.ajax(url).then(function(data){
-// 	console.log(data);
-// 	data.forEach(function({title, date_published, post}){
-// 		$('#news').append(`
-// 			<div class="news_wrapper">
-// 				<div class="news_title">${title}</div>
-// 				<div class="news_date">${date_published}</div>
-// 				<div class="news_post">${post}</div>
-// 			</div>
-// 			`);
-// 		console.log('work');
-// 	});
-// });
-
-
-// 
-
-// var $app = $('.app');
-
-// form_data.forEach(function(datum) {
-// 	var template;
-// 	if (datum.type === "text" || datum.type === "email" || datum.type === "tel" || datum.type === "date") {
-// 		template = lineOfText;
-// 	} else if (datum.type === "textarea") {
-// 		template = commentSection;
-// 	} else if (datum.type === "select") {
-// 		template = selectSeat;
-// 	}
-// 	var html = template(datum);
-// 	$app.append(html);
-// });
-=======
 
 
 
 // // 
 
- 
+
 var $reservation = $('.reservation');
 
 formArray.forEach(function(datum) {
@@ -153,7 +100,6 @@ formArray.forEach(function(datum) {
 	var html = template(datum);
 	$reservation.append(html);
 });
->>>>>>> ef38a9a844ac4dfac891f69322dc57744b444af0
 
 // var $app = $('.app');
 
@@ -169,7 +115,7 @@ formArray.forEach(function(datum) {
 // 	var html = template(datum);
 // 	$app.append(html);
 // });
- 
+
 
 
 
@@ -260,37 +206,6 @@ console.log('work more');
 
 // Function for the bottom copyright date.
 
- 
-function nowSeconds() {
-	var today;
-	var currentSeconds;
-
-	today = new Date();
-	currentSeconds = today.getSeconds();
-
-	var copyrightSeconds = document.querySelector('.copyright')
-
- 	copyrightSeconds.innerHTML = `&copy; ${currentSeconds} Ye Olde.`;
- };
-
- var intID = window.setInterval(nowSeconds, 1000);
-
-
-
- //function nowYear() {
-	 //var today;
-	 //var currentYear;
-
-	 //today = new Date();
-	 //currentYear = today.getFullYear();
-
-	 //var copyrightYear = document.querySelector('.copyright')
-
-  	//copyrightYear.innerHTML = `&copy; ${currentYear} Ye Olde.`;
-  //};
-
-  //var intID = window.setInterval(nowYear, 1000);
-
 // function nowSeconds() {
 // 	var today;
 // 	var currentSeconds;
@@ -320,5 +235,3 @@ function nowYear() {
  };
 
  var intID = window.setInterval(nowYear, 86400000);
-
-
